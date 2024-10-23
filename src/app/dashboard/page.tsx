6917e395fdf2d931f4eb06fb39c49a4abc493ef5
Edgar Montiel <edgarmontiel961@gmail.com>
2024-10-23 11:29:56 -0600

    feat(dashboard): add workspace services to spaces data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
+import { WorkspaceCard } from '@/components/workspaces/workspace-card/workspace-card'
 import { validateUser } from '@/services/auth'
 import { getWorkspaces } from '@/services/workspaces'
-import Image from 'next/image'
 import { redirect } from 'next/navigation'
 import { css } from '~root/styled-system/css'
 
@@ -16,36 +16,7 @@ export default async function Dashboard() {
   return (
     <div className={css({ p: 12, display: 'grid', gridTemplateColumns: 5, gap: 16 })}>
       {workspaces?.map((workspace) => (
-        <article className={css({
-          pos: 'relative',
-          aspectRatio: 'video',
-          w: '100%',
-          color: 'coolGray.25',
-          fontWeight: 'semibold',
-          borderRadius: 4
-        })}>
-          {workspace.backgroundUrl && <figure>
-            <Image
-              fill
-              loading='eager'
-              alt={workspace.name}
-              src={workspace.backgroundUrl}
-              className={css({ objectFit: 'cover', borderRadius: 4 })}
-            />
-          </figure>}
-          <div className={css({
-            pos: 'absolute',
-            top: 0,
-            left: 0,
-            w: '100%',
-            h: '100%',
-            p: 8,
-            borderRadius: 4,
-            background: 'linear-gradient(180deg, rgba(0,0,0,1) 6%, rgba(255,255,255,0.5886729691876751) 91%)'
-          })}>
-            <h2>{workspace.name}</h2>
-          </div>
-        </article>
+        <WorkspaceCard key={workspace.id} workspace={workspace} />
       ))}
     </div>
   )
--- src/services/workspaces/index.ts
+++ src/services/workspaces/index.ts
@@ -4,3 +4,7 @@ import { Workspace } from '@/types/workspace-types'
 export async function getWorkspaces(): Promise<Workspace[]> {
   return await get('/workspaces')
 }
+
+export async function getWorkspaceByUuid(uuid: string): Promise<Workspace> {
+  return await get(`/workspaces/${uuid}`)
+}
