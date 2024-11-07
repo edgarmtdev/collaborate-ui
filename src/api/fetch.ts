c1840e0fa13d36cc6e725c7776755ff1e538afd1
Edgar Montiel <edgarmontiel961@gmail.com>
2024-11-07 13:19:41 -0600

    fix(cookie): adding setCookie func on fetch lib

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -1,4 +1,5 @@
 import { Constant } from '@/const/Constant'
+import { jwtDecode } from 'jwt-decode'
 import { cookies } from 'next/headers'
 
 const getHeaders = () => ({
@@ -34,6 +35,10 @@ export async function post(path: string, data?: unknown) {
       }
     })
 
+    if (response.ok && response.headers.get('Set-Cookie')) {
+      setAuthCookie(response)
+    }
+
     return response.json()
   } catch (error) {
     return error
@@ -68,5 +73,21 @@ export async function del(path: string, data?: unknown) {
     return response.json
   } catch (error) {
     return error
+  }
+}
+
+const setAuthCookie = (response: Response) => {
+  const setCookieHeader = response.headers.get('Set-Cookie')
+
+  if (setCookieHeader) {
+    const token = setCookieHeader.split(';')[0].split('=')[1]
+
+    cookies().set({
+      name: 'Authentication',
+      value: token,
+      secure: true,
+      httpOnly: true,
+      expires: new Date(jwtDecode(token).exp! * 1000)
+    })
   }
 }
--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -2,30 +2,9 @@
 
 import { loginService } from '@/services/auth'
 import type { Credentials } from '@/types/auth-types.d'
-import { jwtDecode } from 'jwt-decode'
-import { cookies } from 'next/headers'
 
 export async function loginAction(credentials: Credentials) {
   const response = await loginService(credentials)
 
-  setAuthCookie(response)
-
   return response
-}
-
-// TODO:  Refactor set cookie in custom fetch api
-const setAuthCookie = (response: Response) => {
-  const setCookieHeader = response.headers.get('Set-Cookie')
-
-  if (setCookieHeader) {
-    const token = setCookieHeader.split(';')[0].split('=')[1]
-
-    cookies().set({
-      name: 'Authentication',
-      value: token,
-      secure: true,
-      httpOnly: true,
-      expires: new Date(jwtDecode(token).exp! * 1000)
-    })
-  }
 }
