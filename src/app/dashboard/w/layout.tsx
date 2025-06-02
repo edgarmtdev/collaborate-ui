4103fb789f75f5fb4e61c8ce29b312593caeda40
Edgar Montiel <61670650+edgarmtdev@users.noreply.github.com>
2025-06-01 21:27:25 -0600

    feat: enhance workspace layout and board component styles

--- src/app/dashboard/w/layout.tsx
+++ src/app/dashboard/w/layout.tsx
@@ -1,10 +1,11 @@
 import { LayoutProps } from '@/types/common'
+import { css } from '~root/styled-system/css'
 
 export default function WorkspaceLayout({
   children
 }: LayoutProps
 ) {
   return (
-    <main>{children}</main>
+    <main className={css({ display: 'flex', flexDirection: 'column', height: '100vh' })}>{children}</main>
   )
 }
---
+++ src/components/boards/board/styles.ts
@@ -0,0 +1,21 @@
+import { css } from "~root/styled-system/css"
+
+export default {
+  board: css({
+    backgroundColor: 'white',
+    width: 272,
+    borderRadius: 10,
+    boxShadow: 'md'
+  }),
+  header: css({
+    backgroundColor: 'primary.700',
+    color: 'white',
+    blur: 10,
+    p: 10,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 8
+  }),
+  title: css({ fontSize: 'md', fontWeight: 'semibold' }),
+  content: css({ flexGrow: 1, p: 14 }),
+  footer: css({ marginTop: '16px', textAlign: 'right' }),
+}
