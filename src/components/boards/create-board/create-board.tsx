8fd31362fcb7fd1a6aedc80f38a362edf5e84377
Edgar Montiel <61670650+edgarmtdev@users.noreply.github.com>
2026-01-02 21:20:03 -0600

    feat(create-board): refactor handleCreateBoard to include response validation and refresh router

--- src/components/boards/create-board/create-board.tsx
+++ src/components/boards/create-board/create-board.tsx
@@ -3,6 +3,7 @@
 import { createNewBoardAction } from "@/app/actions/boards";
 import { Button, Input } from "@/components/ui";
 import { useClickOutside } from "@/hooks";
+import { useRouter } from "next/navigation";
 import { useRef, useState } from "react";
 import styles from "./create-board.styled";
 
@@ -13,11 +14,16 @@ interface CreateBoardProps {
 export function CreateBoard({ workSpaceUuid }: CreateBoardProps) {
   const [showForm, setShowForm] = useState(false);
   const formRef = useRef<HTMLFormElement>(null);
+  const router = useRouter();
   useClickOutside(formRef, () => setShowForm(false), { enabled: true, detectEscapeKey: true });
 
   const handleCreateBoard = async () => {
-    const response = await createNewBoardAction(new FormData(formRef.current!));
-    console.log("Pruebas", response);
+    const response = await createNewBoardAction(new FormData(formRef.current!)) as { uuid?: string } | undefined;
+    if (!response?.uuid) {
+      return;
+    }
+    setShowForm(false);
+    router.refresh();
   }
 
   return (
